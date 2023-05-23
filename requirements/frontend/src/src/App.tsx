e31cd63ce5db6b422a3ed639179b10bcee21148d
Alexander Sabouret <alexandersabouret@gmail.com>
2023-05-23 22:32:48 +0200

    added backend

--- requirements/frontend/src/src/App.tsx
+++ requirements/frontend/src/src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Navbar />
+              <header className="App-header">
+                <h1>Home</h1>
+                <img src={logo} className="App-logo" alt="logo" />
+              </header>
+            </div>
+          }
+        />
+        <Route
+          path="/Scores"
+          element={
+            <div>
+              <Navbar />
+              <header className="App-header">
+                <h1>Scores</h1>
+                <img src={logo} className="App-logo" alt="logo" />
+              </header>
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 }
---
+++ requirements/frontend/src/src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+interface NavbarLinkProps {
+  dest: string;
+  children: any;
+}
+
+function NavbarLink({dest, children}: NavbarLinkProps) {
+  const resolvedPath = useResolvedPath(dest);
+  const isActive = useMatch(resolvedPath.pathname);
+
+  return (
+    <Link to={dest} className={isActive ? "navbar-item navbar-item-active" : "navbar-item"}>
+      {children}
+    </Link>
+  );
+}
+
+export default function Navbar() {
+  return (<nav className="navbar">
+    <NavbarLink dest="/">Home</NavbarLink>
+    <NavbarLink dest="/Scores">Scores</NavbarLink>
+  </nav>);
+}
