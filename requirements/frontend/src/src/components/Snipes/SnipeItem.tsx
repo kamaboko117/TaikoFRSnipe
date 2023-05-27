cb7152761baa1d3a850d677945f5247600648664
Alexander Sabouret <alexandersabouret@gmail.com>
2023-05-27 11:09:40 +0200

    added timestamps to snipelist

--- requirements/frontend/src/src/components/Snipes/SnipeItem.tsx
+++ requirements/frontend/src/src/components/Snipes/SnipeItem.tsx
@@ -6,10 +6,31 @@ export default function SnipeItem({ snipe }: { snipe: Snipe }) {
   return (
     <div className="snipe-item">
       <BeatmapItem beatmap={snipe.beatmap} />
-      <span className="beatmap-snipe">
-        {snipe.sniper.name}
-        {snipe.victim ? `sniped ${snipe.victim.name}` : " claimed 1st place"}
-      </span>
+      <div className="beatmap-snipe">
+        <span className="beatmap-sniper">
+          {snipe.sniper.name}
+        </span>
+        {snipe.victim ? (
+          <>
+          <span> sniped </span>
+          <span className="beatmap-victim">{snipe.victim.name}</span>
+          </>
+        ) : (
+          <span> claimed 1st place</span>
+        )}
+        <br />
+        <span className="beatmap-snipe-date">
+          {/*calculates how long ago*/
+            Math.floor((Date.now() - Date.parse(snipe.timestamp.toString())) / 1000 / 60 / 60 / 24) > 0
+            ? Math.floor((Date.now() - Date.parse(snipe.timestamp.toString())) / 1000 / 60 / 60 / 24) + " days"
+            : Math.floor((Date.now() - Date.parse(snipe.timestamp.toString())) / 1000 / 60 / 60) > 0
+            ? Math.floor((Date.now() - Date.parse(snipe.timestamp.toString())) / 1000 / 60 / 60) + " hours"
+            : Math.floor((Date.now() - Date.parse(snipe.timestamp.toString())) / 1000 / 60) > 0
+            ? Math.floor((Date.now() - Date.parse(snipe.timestamp.toString())) / 1000 / 60) + " minutes"
+            : Math.floor((Date.now() - Date.parse(snipe.timestamp.toString())) / 1000) + " seconds"
+            } ago
+        </span>
+      </div>
     </div>
   );
 }
