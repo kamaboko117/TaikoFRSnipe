823e8dbda6c89b05c2cd0e75043af30f6bfd9117
Alexander Sabouret <alexandersabouret@gmail.com>
2023-05-29 22:36:11 +0200

    fixed bad search module setup

--- requirements/backend/src/src/search/search.module.ts
+++ requirements/backend/src/src/search/search.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SearchController } from './search.controller';
 import { SearchService } from './search.service';
+import { PlayersModule } from '../players/players.module';
+import { BeatmapsModule } from 'src/beatmaps/beatmaps.module';
 
 @Module({
   controllers: [SearchController],
   providers: [SearchService],
+  imports: [PlayersModule, BeatmapsModule],
+
 })
 export class SearchModule {}
