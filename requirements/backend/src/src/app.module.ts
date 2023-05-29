e770552481c47a13e59c956d7e6530f1d71c6a20
Alexander Sabouret <alexandersabouret@gmail.com>
2023-05-29 17:06:22 +0200

    changed app module so that it setups typeorm differently depending on the environment

--- requirements/backend/src/src/app.module.ts
+++ requirements/backend/src/src/app.module.ts
@@ -5,26 +5,41 @@ import { BeatmapsModule } from './beatmaps/beatmaps.module';
 import { ScoresModule } from './scores/scores.module';
 import { PlayersModule } from './players/players.module';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const prodFactory = {
+  type: 'postgres',
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  url: process.env.DATABASE_URL,
+  port: Number(process.env.POSTGRES_PORT),
+  autoLoadEntities: true,
+  synchronize: false,
+};
+
+const devFactory = {
+  type: 'postgres',
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  synchronize: true,
+  autoLoadEntities: true,
+  port: Number(process.env.POSTGRES_PORT),
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        host: process.env.POSTGRES_HOST,
-        port: Number(process.env.POSTGRES_PORT),
-        url: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
+      useFactory: () => (isProduction ? (prodFactory as any) : devFactory),
       inject: [ConfigService],
     }),
     BeatmapsModule,
