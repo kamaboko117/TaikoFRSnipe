950054d58dd7ff032010f5b3abe2082613931deb
Alexander Sabouret <alexandersabouret@gmail.com>
2024-04-27 00:16:46 +0200

    f it, we ball

--- requirements/backend/src/src/app.module.ts
+++ requirements/backend/src/src/app.module.ts
@@ -20,7 +20,7 @@ const prodFactory = {
   url: process.env.DATABASE_URL,
   port: Number(process.env.POSTGRES_PORT),
   autoLoadEntities: true,
-  synchronize: false,
+  synchronize: true,
 };
 
 const devFactory = {
