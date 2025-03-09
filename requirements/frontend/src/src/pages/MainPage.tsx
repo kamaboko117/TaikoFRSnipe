8b1fc19188816ee9e0b2089c844092e5b803f3b0
Alexander Sabouret <asaboure@student.42.fr>
2025-03-09 11:21:58 +0100

    refactor: simplify MainPage component and improve state management

--- requirements/frontend/src/src/pages/MainPage.tsx
+++ requirements/frontend/src/src/pages/MainPage.tsx
@@ -1,30 +1,22 @@
-import React, { useState } from "react";
-import Navbar from "../components/Navbar/Navbar";
-import ManualUpdateForm from "../components/Beatmaps/manualUpdateForm";
-import { Beatmap, Snipe } from "../types/api";
-import SnipeList from "../components/Snipes/SnipeList";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ManualUpdateForm from "../components/Beatmaps/manualUpdateForm";
+import Navbar from "../components/Navbar/Navbar";
+import SnipeList from "../components/Snipes/SnipeList";
+import { Snipe } from "../types/api";
 const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
 
 export default function MainPage() {
-  const [snipes, setSnipes] = useState([] as Snipe[]);
+  const [snipes, setSnipes] = useState<Snipe[]>([]);
   const navigate = useNavigate();
-  React.useEffect(() => {
-    let snipesArray = [] as Snipe[];
+  useEffect(() => {
     fetch(`${REACT_APP_API_URL}/snipes/latest/10`)
       .then((res) => res.json())
       .then((data) => {
         if (!data.error) {
           setSnipes(data);
-          snipesArray = data;
         }
-      })
-      .then(() => {
-        let beatmapArray = [] as Beatmap[];
-        for (let i = 0; i < snipesArray.length; i++) {
-          beatmapArray.push(snipesArray[i].beatmap);
-        }
-      });
+      })  
   }, []);
 
   return (
