ab3bc5e73b0815526ce7c061e2516c9ddb8055cf
Ubuntu <ubuntu@vps-533687ea.vps.ovh.net>
2025-10-04 13:59:59 +0000

    feat(backend): change default port

--- requirements/backend/src/src/main.ts
+++ requirements/backend/src/src/main.ts
@@ -1,15 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix("/api");
   app.enableCors();
-  const PORT = process.env.PORT || 3000;
-  const herokuKeepAwake = require("heroku-keep-awake");
-  herokuKeepAwake.wakeDyno("https://tfrs-backend.herokuapp.com/", {stopTimes: {start: "00:00", end: "00:00"}});
+  const PORT = process.env.PORT || 4000;
   await app.listen(PORT);
 }
 bootstrap();
